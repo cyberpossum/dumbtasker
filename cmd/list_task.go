98bde1e6b4bec96f2f7b4d43d771a03231d3b153
cyberpossum <61364422+cyberpossum@users.noreply.github.com>
2020-06-12 22:07:19 +0300

    Add colored output to the list function
    For better readability, enable color output with a key to suppress and
    fall back to the monochrome mode.

--- cmd/list_task.go
+++ cmd/list_task.go
@@ -6,13 +6,43 @@ import (
 
 	"github.com/cyberpossum/dumbtasker/database"
 	"github.com/cyberpossum/dumbtasker/dto"
+	"github.com/fatih/color"
+)
+
+const (
+	_redCutoff    = 2 * time.Hour
+	_yellowCutoff = 6 * time.Hour
 )
 
 type listTask struct {
 	common
 
-	Due smartTime `long:"due" description:"Tasks with due date before the specified"`
-	All bool      `short:"a" long:"all" description:"Show all tasks including completed"`
+	Due     smartTime `long:"due" description:"Tasks with due date before the specified"`
+	All     bool      `short:"a" long:"all" description:"Show all tasks including completed"`
+	NoColor bool      `long:"no-color" description:"suppress color output"`
+}
+
+func (l *listTask) formatOutput(t *dto.Task) (string, string) {
+	idStr := fmt.Sprintf("%08d", t.ID)
+	dueStr := fmt.Sprintf("%v", t.Due)
+	if l.NoColor {
+		return idStr, dueStr
+	}
+	dueFmt := color.GreenString
+	switch {
+	case t.Due.Before(time.Now()):
+		dueFmt = color.HiRedString
+	case t.Due.Before(time.Now().Add(_redCutoff)):
+		dueFmt = color.RedString
+	case t.Due.Before(time.Now().Add(_yellowCutoff)):
+		dueFmt = color.YellowString
+	}
+
+	if t.Status == dto.Closed {
+		idStr = color.HiBlackString(idStr)
+	}
+
+	return idStr, dueFmt(dueStr)
 }
 
 // Execute implements flags.Commander
@@ -42,7 +72,8 @@ func (l *listTask) Execute(args []string) error {
 	}
 
 	for _, tt := range tasks {
-		fmt.Printf("%08d\t%q:\t%v\n", tt.ID, tt.Description, tt.Due)
+		id, fdue := l.formatOutput(&tt)
+		fmt.Printf("%v\t%q:\t%v\n", id, tt.Description, fdue)
 	}
 
 	return nil
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/araddon/dateparse v0.0.0-20200409225146-d820a6159ab1
+	github.com/fatih/color v1.9.0
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/jinzhu/gorm v1.9.12
