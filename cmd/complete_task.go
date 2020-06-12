abc0b93b00b6a76ec1ad13934e8a67414cf3f86a
cyberpossum <61364422+cyberpossum@users.noreply.github.com>
2020-06-13 00:41:17 +0300

    Implement the delete command
    Do a small refactor and implement delete task operation. The operation
    does not delete the db record from the table, only marks it as deleted.

--- cmd/complete_task.go
+++ cmd/complete_task.go
@@ -1,10 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"strconv"
-
-	"github.com/cyberpossum/dumbtasker/database"
 	"github.com/cyberpossum/dumbtasker/dto"
 )
 
@@ -18,27 +14,5 @@ type completeTask struct {
 
 // Execute executes the command, adding a task
 func (a *completeTask) Execute([]string) error {
-	id, err := strconv.Atoi(a.PosArgs.ID)
-	if err != nil {
-		return fmt.Errorf("invalid id: %w", err)
-	}
-
-	db, err := database.OpenDB(a.DBType, a.ConnStr)
-	if err != nil {
-		return fmt.Errorf("opening database: %w", err)
-	}
-	defer db.Close()
-
-	var t dto.Task
-
-	if err := db.First(&t, id).Error; err != nil {
-		return fmt.Errorf("error looking up record with ID %d: %w", id, err)
-	}
-
-	t.Status = dto.Closed
-	if err := db.Save(t).Error; err != nil {
-		return fmt.Errorf("updating record: %w", err)
-	}
-
-	return nil
+	return changeTaskStatus(a.DBType, a.ConnStr, a.PosArgs.ID, []dto.TaskStatus{dto.Open}, dto.Closed)
 }
